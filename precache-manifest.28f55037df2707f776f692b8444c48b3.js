3c8ab8abfbad9229286ac2b20503d43bc3a8517d
your username <amalalmomani19@gmail.com>
2020-06-29 22:14:21 +0300

    Updates

---
+++ precache-manifest.28f55037df2707f776f692b8444c48b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f997b4dd1560cfa103e15249e0367e01",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "35b5d6d321b5e73a799b",
+    "url": "/resty/static/css/main.acdc24bb.chunk.css"
+  },
+  {
+    "revision": "3c3fe42eb5bed02bd8ed",
+    "url": "/resty/static/js/2.62d548b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.62d548b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b5d6d321b5e73a799b",
+    "url": "/resty/static/js/main.b121bf79.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
